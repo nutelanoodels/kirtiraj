3624b1fe14a44aeb47067621b502e01ce9061770
sevpuri <168060898+nutelanoodels@users.noreply.github.com>
2026-01-03 18:00:16 +0530

    Temporary admin bootstrap

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,15 +1,21 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.http import HttpResponse
+# core/urls.py
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 
+def bootstrap_admin(request):
+    if User.objects.filter(username="jaymin").exists():
+        return HttpResponse("Admin already exists")
 
-
-
+    User.objects.create_superuser(
+        username="jaymin",
+        email="jaymin@example.com",
+        password="lalolalo003"
+    )
+    return HttpResponse("Admin created successfully")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    
+    path("bootstrap-admin/", bootstrap_admin),
     path("api/", include("products.urls")),
     path("api/orders/", include("orders.urls")),
 ]
