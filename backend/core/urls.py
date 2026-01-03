44f39d0ce6bb799f72cf1d167b8a2bd88c59afc8
sevpuri <168060898+nutelanoodels@users.noreply.github.com>
2026-01-03 18:03:01 +0530

    Fix urls imports and admin bootstrap

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,8 @@
-# core/urls.py
-from django.contrib.auth.models import User
+from django.contrib import admin
+from django.urls import path, include
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+
 
 def bootstrap_admin(request):
     if User.objects.filter(username="jaymin").exists():
@@ -13,9 +15,10 @@ def bootstrap_admin(request):
     )
     return HttpResponse("Admin created successfully")
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("bootstrap-admin/", bootstrap_admin),
+    path("bootstrap-admin/", bootstrap_admin),  # TEMP – remove later
     path("api/", include("products.urls")),
     path("api/orders/", include("orders.urls")),
 ]
