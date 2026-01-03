274531801ffb4334ca8bcdb4bd618410df27a371
sevpuri <168060898+nutelanoodels@users.noreply.github.com>
2026-01-03 18:34:12 +0530

    Remove admin bootstrap route

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,24 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-from django.contrib.auth.models import User
-
-
-def bootstrap_admin(request):
-    if User.objects.filter(username="jaymin").exists():
-        return HttpResponse("Admin already exists")
-
-    User.objects.create_superuser(
-        username="jaymin",
-        email="jaymin@example.com",
-        password="lalolalo003"
-    )
-    return HttpResponse("Admin created successfully")
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("bootstrap-admin/", bootstrap_admin),
     path("api/", include("products.urls")),
     path("api/orders/", include("orders.urls")),
 ]
