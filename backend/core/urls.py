6204a3243271a78e542299a525e110e0c530cd00
sevpuri <168060898+nutelanoodels@users.noreply.github.com>
2026-01-03 17:54:39 +0530

    Remove bootstrap admin endpoint

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,21 +4,12 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 
 
-def bootstrap_admin(request):
-    if User.objects.filter(username="jaymin").exists():
-        return HttpResponse("Admin already exists")
 
-    User.objects.create_superuser(
-        username="jaymin",
-        email="jaymin@example.com",
-        password="lalolalo003"
-    )
-    return HttpResponse("Admin created successfully")
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("bootstrap-admin/", bootstrap_admin),  # TEMP
+    
     path("api/", include("products.urls")),
     path("api/orders/", include("orders.urls")),
 ]
