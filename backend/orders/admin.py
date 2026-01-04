f998f86dafc0de4fe696792aa4f7ca5fa1d7f327
sevpuri <168060898+nutelanoodels@users.noreply.github.com>
2026-01-04 22:23:20 +0530

    Make order items readable on mobile admin

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -1,26 +1,15 @@
 from django.contrib import admin
-from django.utils.html import format_html
 from .models import Order, OrderItem
-
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     extra = 0
 
-
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "phone", "total_amount", "print_link")
+    list_display = ("id", "name", "phone", "total_amount", "created_at")
     ordering = ("-created_at",)
     inlines = [OrderItemInline]
-
-    def print_link(self, obj):
-        return format_html(
-            '<a href="/api/orders/{}/print/" target="_blank">🖨 Print</a>',
-            obj.id
-        )
-
-    print_link.short_description = "Print"
 
     class Media:
         css = {
