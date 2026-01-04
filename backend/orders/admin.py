287fc88333ab5767cb53eb4701b6ebe00bd091e5
sevpuri <168060898+nutelanoodels@users.noreply.github.com>
2026-01-04 22:10:27 +0530

    Fix admin print button name

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -1,23 +1,26 @@
 from django.contrib import admin
-from .models import Order, OrderItem
 from django.utils.html import format_html
+from .models import Order, OrderItem
+
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     extra = 0
 
-def print_order(self, obj):
-    return format_html(
-        '<a href="/admin/orders/order/{}/print/" target="_blank">🖨 Print</a>',
-        obj.id
-    )
-
-print_order.short_description = "Print"
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "phone", "total_amount", "print_order")
+    list_display = ("id", "name", "phone", "total_amount", "print_link")
     ordering = ("-created_at",)
     inlines = [OrderItemInline]
+
+    def print_link(self, obj):
+        return format_html(
+            '<a href="/api/orders/{}/print/" target="_blank">🖨 Print</a>',
+            obj.id
+        )
+
+    print_link.short_description = "Print"
 
     class Media:
         css = {
